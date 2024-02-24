8c1f4a60facae1fe08f5c63edbcfa2892e1a1793
BGWEB <121228936+BGWEB08@users.noreply.github.com>
2024-02-24 20:58:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let typed = new Typed(".typing-text", {
+        strings: ["WEB DEVELOPER.", "WEB DESIGNER.", "and", "PROJECT MANAGER."],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true 
+    });
+});
