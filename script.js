aedbac590acb3e28c61576a8d5666b450b0cf112
BGWEB <121228936+BGWEB08@users.noreply.github.com>
2024-02-24 21:43:58 +0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     let typed = new Typed(".typing-text", {
-        strings: ["WEB DEVELOPER.", "WEB DESIGNER.", "and", "PROJECT MANAGER."],
+        strings: ["WEB DEVELOPER.", "WEB DESIGNER.", "PROJECT MANAGER."],
         typeSpeed: 150,
         backSpeed: 150,
         loop: true 
